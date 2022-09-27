c34db33264144db62367a4aab0adc395d0de1082
khadzharZ <khadzharzakhaddinova@gmail.com>
2022-09-27 19:50:28 -0400

    second commit

--- src/class03B14/Class03.java
+++ src/class03B14/Class03.java
@@ -3,5 +3,6 @@ package class03B14;
 public class Class03 {
 public static void main(String[]args) {
 	System.out.println("hey this is a git class");
+	System.out.println("hey this is a change in my project");
 }
 }
